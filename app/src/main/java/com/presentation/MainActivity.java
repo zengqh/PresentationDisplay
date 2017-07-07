0648b44a01d49666488de44ca17cba5e6575283b
zengqh <zengqh.mansion@gmail.com>
2017-07-07 15:59:18 +0800

    first commit

---
+++ app/src/main/java/com/presentation/MainActivity.java
@@ -0,0 +1,37 @@
+package com.presentation;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.display.DisplayManager;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 主屏
+        setContentView(new MainGLSurfaceView(this));
+
+        // 副屏显示
+        PresentationDisplay present_display = createPresentDisplay();
+        if (present_display != null) present_display.showView();
+    }
+
+    private PresentationDisplay createPresentDisplay() {
+        DisplayManager display_mgr = (DisplayManager) getSystemService(Context.DISPLAY_SERVICE);
+        Display[] displays = display_mgr.getDisplays(DisplayManager.DISPLAY_CATEGORY_PRESENTATION);
+
+        if (displays != null && displays.length > 0) {
+            PresentationDisplay present_display = new PresentationDisplay(getApplicationContext(), displays[0]);
+            present_display.getWindow().setType(WindowManager.LayoutParams.TYPE_TOAST);
+
+            return present_display;
+        }
+
+        return null;
+    }
+}
